b225fd82869a7c74313d4c119bd21114ab9a470e
Khaliph Green <kgreen375@gmail.com>
2024-06-10 22:26:15 -0500

    Image table association added

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -8,7 +8,13 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Spot.hasMany(models.Image, {
+        foreignKey: "imageableId",
+        constraints: false,
+        scope: {
+          imageableType: "Spot",
+        },
+      });
     }
   }
   Spot.init(
