0f68c93915f63c1959b1d6afbf562ee06b1795cc
Khaliph Green <kgreen375@gmail.com>
2024-06-15 14:27:10 -0500

    Removed unnecessary route

--- backend/routes/api/images.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { requireAuth } = require("../../utils/auth");
-const { User, Spot, Image, Review, Booking } = require("../../db/models");
-
-
-
-module.exports = router;
