57775ca97dd65c7883d36fd90cbf4b301aba0478
Khaliph Green <kgreen375@gmail.com>
2024-06-15 12:14:58 -0500

    Delete Spot/Review Image Route Added

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,7 +8,7 @@ const bookingsRouter = require("./bookings.js");
 const { restoreUser } = require("../../utils/auth.js");
 
 const { Image, Review, Spot } = require("../../db/models");
-const { requireAuth } = require("../../utils/validation.js")
+const { requireAuth } = require("../../utils/validation.js");
 
 router.use(restoreUser);
 
@@ -18,10 +18,69 @@ router.use("/spots", spotsRouter);
 router.use("/reviews", reviewsRouter);
 router.use("/bookings", bookingsRouter);
 
+//Test Route
 // router.post("/test", (req, res) => {
 //   res.json({ requestBody: req.body });
 // });
 
+//Delete Spot Image
+router.delete("/spot-images/:imageId", requireAuth, async (req, res, _next) => {
+  const image = await Image.findByPk({
+    where: {
+      imageableId: req.params.imageId,
+      imageableType: "Spot",
+    },
+  });
 
+  if (!image) {
+    return res.status(404).json({ message: "Spot image couldn't be found" });
+  }
+
+  const spot = await Spot.findByPk(image.imageableId);
+  if (spot.ownerId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: "You do not have permission to delete this image" });
+  }
+
+  await image.destroy();
+
+  return res.status(200).json({
+    message: "Successfully deleted",
+  });
+});
+
+//Delete Review Spot
+router.delete(
+  "/review-images/:imageId",
+  requireAuth,
+  async (req, res, _next) => {
+    const image = await Image.findByPk({
+      where: {
+        imageableId: req.params.imageId,
+        imageableType: "Review",
+      },
+    });
+
+    if (!image) {
+      return res
+        .status(404)
+        .json({ message: "Review image couldn't be found" });
+    }
+
+    const review = await Review.findByPk(image.imageableId);
+    if (review.userId !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "You do not have permission to delete this image" });
+    }
+
+    await image.destroy();
+
+    return res.status(200).json({
+      message: "Successfully deleted",
+    });
+  }
+);
 
 module.exports = router;
