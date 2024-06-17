64cd9771da6fba70b6ba1f71deddc85a0b02897e
Khaliph Green <kgreen375@gmail.com>
2024-06-16 23:32:41 -0500

    Updated SignUp to account for duplicate user

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -9,12 +9,41 @@ const router = express.Router();
 
 const { validateSignUp } = require("../../utils/validation");
 
+//Sign Up User
 router.post("/", validateSignUp, async (req, res, _next) => {
   const { firstName, lastName, username, email, password } = req.body;
 
   const hashedPassword = bcrypt.hashSync(password, saltRounds);
 
-  const user = await User.create({
+  const existingUser = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: username,
+        email: email,
+      },
+    },
+  });
+
+  if (existingUser) {
+    if (existingUser.email === email) {
+      return res.status(500).json({
+        message: "User already exists",
+        errors: {
+          email: "User with that email already exists",
+        },
+      });
+    }
+    if (existingUser.username === username) {
+      return res.status(500).json({
+        message: "User already exists",
+        errors: {
+          email: "User with that email already exists",
+        },
+      });
+    }
+  }
+
+  const newUser = await User.create({
     firstName,
     lastName,
     username,
@@ -23,14 +52,14 @@ router.post("/", validateSignUp, async (req, res, _next) => {
   });
 
   const safeUser = {
-    id: user.id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
-    username: user.username,
+    id: newUser.id,
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    email: newUser.email,
+    username: newUser.username,
   };
 
-  await setTokenCookie(res, safeUser);
+  setTokenCookie(res, safeUser);
 
   return res.status(201).json({
     user: safeUser,
